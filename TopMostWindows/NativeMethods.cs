7d24071954389ec1e5429c964bb05c770e66ca57
kamikuzu <kamikuzu@outlook.com>
2017-04-14 14:34:38 +0900

    add project.

---
+++ TopMostWindows/NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopMostWindows
+{
+    /// <summary>
+    /// this class is provided "NativeMethods".
+    /// </summary>
+    public sealed class NativeMethods
+    {
+
+    }
+}
---
+++ TopMostWindows/frmStart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopMostWindows
+{
+    /// <summary>
+    /// this form is instanciated when application start.
+    /// </summary>
+    public partial class frmStart : Form
+    {
+        public frmStart()
+        {
+            InitializeComponent();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                
+                CreateParams cp = base.CreateParams;
+                return base.CreateParams;
+
+            }
+        }
+    }
+}
